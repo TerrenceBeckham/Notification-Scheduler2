bf3f2b446c026abc48a8b75ba6ffa61fbd583bb4
Terrence Beckham <https://github.com/TerrenceBeckham>
2019-04-12 05:47:35 +0200

    Implemented the jobService, setting up the notification channel, and building the intent.

--- app/src/main/java/com/afrodroid/notificationscheduler2/NotificationJobService.java
+++ app/src/main/java/com/afrodroid/notificationscheduler2/NotificationJobService.java
@@ -1,12 +1,18 @@
 package com.afrodroid.notificationscheduler2;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.job.JobParameters; 
+import android.app.PendingIntent;
+import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 
 public class NotificationJobService extends JobService {
-    NotificationManager mNotificationManager;
+    NotificationManager mNotifyManager;
     //Notification Channel ID
     public static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
 
@@ -15,13 +21,27 @@ public class NotificationJobService extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters params) {
+        createNotificationChannel();
+        //Set up the notification content intent to launch the app when clicked.
+        PendingIntent contentPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,MainActivity.class),
+        PendingIntent.FLAG_UPDATE_CURRENT);
 
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle(this.getString(R.string.job_service))
+                .setContentText(this.getString(R.string.job_ran_to_completion))
+                .setContentIntent(contentPendingIntent)
+                .setSmallIcon(R.drawable.ic_job_running)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true);
+        //Make notificationChannel
+        mNotifyManager.notify(0, builder.build());
         return false;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
-        return false;
+        return true;
     }
 
 
@@ -32,7 +52,16 @@ public class NotificationJobService extends JobService {
         //Notification channels are only available in Oreo and higher.
         //So, add a check on SDK version.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            //Make a notification channel
+            //Make a notification channel with all the parameters.
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID, "Job service notification", NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Job Service");
+
+            //Build the NotificationChannel
+            mNotifyManager.createNotificationChannel(notificationChannel);
         }
     }
 }
