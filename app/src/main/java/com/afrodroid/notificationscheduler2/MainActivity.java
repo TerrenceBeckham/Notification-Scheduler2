d9c763314985732865c80c0e2e21a4fc12b1cd3a
Terrence Beckham <https://github.com/TerrenceBeckham>
2019-04-12 07:07:26 +0200

    Made the JobScheduler and JobInfo objects.

--- app/src/main/java/com/afrodroid/notificationscheduler2/MainActivity.java
+++ app/src/main/java/com/afrodroid/notificationscheduler2/MainActivity.java
@@ -1,10 +1,17 @@
 package com.afrodroid.notificationscheduler2;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private JobScheduler mScheduler;
+    public static final int JOB_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,9 +19,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sheduleJob(View view) {
+    public void scheduleJob(View view) {
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        RadioGroup netWorkOptions = findViewById(R.id.networkOptions);
+        //get selected netWork id
+        int selectNetworkID = netWorkOptions.getCheckedRadioButtonId();
+
+        // set  network option to the default
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+
+        switch (selectNetworkID) {
+            case R.id.noNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+                break;
+            case R.id.anyNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+                break;
+            case R.id.wifiNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
+                break;
+        }
+
+        ComponentName serviceName = new ComponentName(getPackageName(), NotificationJobService.class.getName());
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName)
+                //call setRequiredNetworkType() on the JobInfoBuilder obj.
+                .setRequiredNetworkType(selectedNetworkOption);
+
+        //Call schedule() on the JobScheduler object using the build method.
+        JobInfo myJobInfo = builder.build();
+        mScheduler.schedule(myJobInfo);
+        Toast.makeText(this, "Job Scheduled, job will run when" + "The constants are met."
+                ,Toast.LENGTH_SHORT ).show();
     }
 
     public void cancelJobs(View view) {
+        if (mScheduler != null) {
+            mScheduler.cancelAll();
+            mScheduler = null;
+            Toast.makeText(this, "Jobs cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
